42c8ccec68340a2f356178aaf9f2173fbfb27849
Pedro <Pedro@Pedro>
2013-11-27 13:44:12 -0300

    problemas arrumados. Implementar banco de dados.

---
+++ src/Conexao/comCli.java
@@ -0,0 +1,44 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Conexao;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Pedro
+ */
+public class comCli extends Thread {
+// Flag que indica quando se deve terminar a execução.
+
+    private static boolean done = false;
+
+    private Socket conexao;
+    private String comando;
+    
+
+    public comCli(Socket conexao, String comando) {
+        this.conexao = conexao;
+        this.comando = comando;
+        try {
+            this.conexao = new Socket("127.0.0.1", 2222);
+            PrintStream saida = new PrintStream(this.conexao.getOutputStream());
+            saida.println(this.comando);
+            
+        } catch (IOException e) {
+            System.out.println("IOException: " + e);
+        }
+    }
+}
---
+++ src/testes/teste1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testes;
+
+import Conexao.comCli;
+
+/**
+ *
+ * @author Pedro
+ */
+public class teste1 {
+    public static void main(String args[]){
+        
+    }
+}
